1c2d698baac325e457082d1b82881c63887bb15a
Alice Boxhall <aboxhall@chromium.org>
2015-07-03 10:58:56 -0700

    Always focus text inputs

--- keyboard-focus.js
+++ keyboard-focus.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     var hadKeyboardEvent = false;
     var focusedElementObserver = null;
-    var lastState = {};
 
-    function compareAndUpdateLastState(el, p) {
-        if (!(p in el))
-            return;
-
-        if (p in lastState && el[p] !== lastState[p])
-            document.body.setAttribute("using-keyboard", "");
-        lastState[p] = el[p];
+    function isTextInput(el) {
+        var hasTextboxRole = el.getAttribute("role") === "textbox",
+            isTextSelectable = false;
+        try {
+            isTextSelectable = typeof el.selectionStart !== "undefined";
+        } catch (e) {
+            /* nope, it isn't... */
+        }
+        return isTextSelectable || hasTextboxRole;
     }
 
     document.body.addEventListener("keydown", function(e) {
@@ -17,41 +18,12 @@ document.addEventListener("DOMContentLoaded", function() {
         setTimeout(function() { hadKeyboardEvent = false; }, 0);
     }, true);
 
-    document.body.addEventListener("keyup", function(e) {
-        // If the focused element gets a character data or selection mutation from the keyboard,
-        // set using-keyboard even if it was focused via a pointing device.
-        compareAndUpdateLastState(e.target, "value");
-        compareAndUpdateLastState(e.target, "selectionStart");
-    }, true);
-
     document.body.addEventListener("focus", function(e) {
-        if (hadKeyboardEvent)
+        if (hadKeyboardEvent || isTextInput(e.target))
             document.body.setAttribute("using-keyboard", "");
-
-        // If the focused element gets a character data mutation from the keyboard,
-        // set using-keyboard even if it was focused via a pointing device.
-        focusedElementObserver = new MutationObserver(function(mutations) {
-            console.log("observed mutation");
-            if (hadKeyboardEvent && !document.body.hasAttribute("using-keyboard"))
-                document.body.setAttribute("using-keyboard", "");
-        });
-        console.log("observing", e.target);
-        focusedElementObserver.observe(e.target, {attributes: true, characterData: true, childList: true});
-
-        if ("value" in e.target)
-            lastState.value = e.target.value;
-        if ("selectionStart" in e.target)
-            lastState.selectionStart = e.target.selectionStart;
     }, true);
 
     document.body.addEventListener("blur", function(e) {
         document.body.removeAttribute("using-keyboard");
-        if (focusedElementObserver !== null) {
-            console.log("removing focusedElementObserver");
-            focusedElementObserver.disconnect();
-            focusedElementObserver = null;
-        }
-        for (var p in lastState)
-            delete lastState[p];
     }, true);
 });
