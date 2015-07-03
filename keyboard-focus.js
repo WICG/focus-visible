f8ade529fb004deae5d7c2b2224fa149bd3e0ba7
Alice Boxhall <aboxhall@chromium.org>
2015-07-03 16:00:09 -0700

    Implement whitelist and update testpage text

--- keyboard-focus.js
+++ keyboard-focus.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var hadKeyboardEvent = false;
-    var focusedElementObserver = null;
+    var hadKeyboardEvent = false,
+        showFocusRingWhitelist = [ "input:not([type])",
+                                   "input[type=text]",
+                                   "input[type=number]",
+                                   "input[type=date]",
+                                   "input[type=time]",
+                                   "input[type=datetime]",
+                                   "textarea",
+                                   "[role=textbox]",
+                                   "[show-focus]"].join(",");
 
-    function isTextInput(el) {
-        var hasTextboxRole = el.getAttribute("role") === "textbox",
-            isTextSelectable = false;
-        try {
-            isTextSelectable = typeof el.selectionStart !== "undefined";
-        } catch (e) {
-            /* nope, it isn't... */
-        }
-        return isTextSelectable || hasTextboxRole;
+    function matchesSelector(el, selector) {
+        if (el.matchesSelector)
+            return el.matchesSelector(selector);
+        if (el.webkitMatchesSelector)
+            return el.webkitMatchesSelector(selector);
+        if (el.mozMatchesSelector)
+            return el.mozMatchesSelector(selector);
+        if (el.msMatchesSelector)
+            return el.msMatchesSelector(selector);
+        return false;
+    }
+
+    function shouldShowFocusRing(el) {
+        return matchesSelector(el, showFocusRingWhitelist);
     }
 
     document.body.addEventListener("keydown", function(e) {
@@ -19,11 +32,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }, true);
 
     document.body.addEventListener("focus", function(e) {
-        if (hadKeyboardEvent || isTextInput(e.target))
-            document.body.setAttribute("show-focus-ring", "");
+        if (hadKeyboardEvent || shouldShowFocusRing(e.target))
+            document.body.setAttribute("show-focus", "");
     }, true);
 
     document.body.addEventListener("blur", function(e) {
-        document.body.removeAttribute("show-focus-ring");
+        document.body.removeAttribute("show-focus");
     }, true);
 });
