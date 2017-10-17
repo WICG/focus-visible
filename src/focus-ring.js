15eb80df780c0f1611db621328748b3f66deb029
Rob Dodson <robdodson@users.noreply.github.com>
2017-10-17 09:31:00 -0700

    Fix missing activeElement. Fixes #56 (#58)

--- src/focus-ring.js
+++ src/focus-ring.js
@@ -118,6 +118,10 @@ function init() {
    * to which it was previously applied.
    */
   function onWindowFocus() {
+    // When removing the activeElement from DOM it's possible IE11 is in state
+    // document.activeElement === null
+    if (!document.activeElement)
+      return;
     if (document.activeElement == elWithFocusRing)
       addFocusRingClass(elWithFocusRing);
 
@@ -129,6 +133,10 @@ function init() {
    * focus-ring class.
    */
   function onWindowBlur() {
+    // When removing the activeElement from DOM it's possible IE11 is in state
+    // document.activeElement === null
+    if (!document.activeElement)
+      return;
     if (classList(document.activeElement).contains('focus-ring')) {
       // Keep a reference to the element to which the focus-ring class is applied
       // so the focus-ring class can be restored to it if the window regains
