95d9ae96417e796aa59167417a31a35f834efe87
Rob Dodson <robdodson@google.com>
2017-11-14 20:28:10 -0800

    Add fixed up version of focus-ring

--- test/specs/input-file.js
+++ test/specs/input-file.js
@@ -10,6 +10,8 @@ describe('<input type="file">', function() {
     return matchesKeyboard();
   });
 
+  // Note: Skipping this test (though it currently passes) because it opens
+  // a file chooser dialog and I don't want it interfering with other tests.
   it.skip('should NOT apply .focus-ring on mouse focus', function() {
     return matchesMouse(false);
   });
