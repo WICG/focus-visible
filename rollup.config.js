90848ff57d820cf1ae9f59231acf1ef8dc69b0e4
Rob Dodson <robdodson@google.com>
2017-11-17 10:23:35 -0800

    Remove unused rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,7 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-
 export default {
   input: 'src/focus-ring.js',
   output: {
     file: 'dist/focus-ring.js',
     format: 'umd'
-  },
-  plugins: [resolve({ jsnext: true, main: true }), commonjs()]
+  }
 };
